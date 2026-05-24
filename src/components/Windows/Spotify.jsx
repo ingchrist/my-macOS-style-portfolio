6faa814fa8a6f8fd13a79cb445f05d2be92036dd
ingchrist <ingchrist@github.com>
2026-05-24 21:57:04 +0000

    modify spotify.jsx

--- src/components/Windows/Spotify.jsx
+++ src/components/Windows/Spotify.jsx
@@ -26,7 +26,7 @@ const Spotify = ({
           width="100%"
           height="352"
           frameBorder="0"
-          allowfullscreen=""
+          allowFullScreen
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
         ></iframe>
